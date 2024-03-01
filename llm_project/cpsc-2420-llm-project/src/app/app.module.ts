6225d50b17db7338b326b618c7495d64e3730bbe
shama2025 <2003mashaffer@gmail.com>
2024-03-01 15:38:09 -0500

    Create Frontend
    *Create new Angular App
    *Create main page component
    *Create main page service

---
+++ llm_project/cpsc-2420-llm-project/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainPageComponent 
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
